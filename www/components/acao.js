00f47223d2076e65847a3570a18bbaad14635e63
GustavoBibiano <gu.bibiano@hotmail.com>
2019-08-10 08:29:09 +0900

    QR cod

--- www/components/acao.js
+++ www/components/acao.js
@@ -19,3 +19,36 @@ $(document).on("click","#alerta",function(){
 
   navigator.notification.confirm("Escolha uma opção",retorno2, "Titulo",['Sim','Não']);
 });
+
+$(document).on("click","#vibrar",function(){
+        navigator.vibrate([3000]);
+});
+
+
+$(document).on("click","#codigo",function()
+{
+  cordova.plugins.barcodeScanner.scan(
+      function (result) {
+         if(result.text == 280720550) {
+           $(location).attr("href","pedroguinha157.html");
+         }
+      },
+      function (error) {
+          alert("Scanning failed: " + error);
+      },
+      {
+          preferFrontCamera : false, // iOS and Android
+          showFlipCameraButton : true, // iOS and Android
+          showTorchButton : true, // iOS and Android
+          torchOn: false, // Android, launch with the torch switched on (if available)
+          saveHistory: true, // Android, save scan history (default false)
+          prompt : "Place a barcode inside the scan area", // Android
+          resultDisplayDuration: 500, // Android, display scanned text for X ms. 0 suppresses it entirely, default 1500
+          formats : "QR_CODE,PDF_417,CODE_39", // default: all but PDF_417 and RSS_EXPANDED
+          orientation : "portrait", // Android only (portrait|landscape), default unset so it rotates with the device
+          disableAnimations : true, // iOS
+          disableSuccessBeep: false // iOS and Android
+      }
+  )
+
+});
