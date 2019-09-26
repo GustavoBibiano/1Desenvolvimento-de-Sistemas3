34343c5976e11fe925e2add2c855fd46fed683d9
GustavoBibiano <gu.bibiano@hotmail.com>
2019-09-26 09:48:29 +0900

    Project's Initial Commit

--- www/components/acao.js
+++ www/components/acao.js
@@ -1,54 +1,49 @@
-// This is a JavaScript file
-function retorno(){
+$(document).on("click","#camera", function(){
+  navigator.camera.getPicture(onSuccess, onFail, 
+  { quality: 76,  
+  destinationType: Camera.DestinationType.FILE_URI,
+  correctOrientation:true,
+  saveToPhotoAlbum:true
+});
 
-}
-  function retorno2(buttonIndex) {
-    if(buttonIndex == 1){
-      navigator.notification.alert("Sim");
-      navigator.notification.beep(1);
-  
-  }else{
-      navigator.notification.alert("Não");
-      navigator.notification.beep(2);
+  function onSuccess(imageURI) {
+    var image = document.getElementById('imagem');
+    image.src = imageURI;
+
   }
-}
-$(document).on("click","#alerta",function(){
-
-  //navigator.notification.alert("Foi",retorno,"Aviso1","Ok");
-  //navigator.notification.beep(2);
-
-  navigator.notification.confirm("Escolha uma opção",retorno2, "Titulo",['Sim','Não']);
+  function onFail(message) {
+    navigator.notification.alert('Erro ao capturar a imagem: ' + message);
+  }
 });
 
-$(document).on("click","#vibrar",function(){
-        navigator.vibrate([3000]);
+$(document).on("change","#escala_cinza",function(){
+
+  var cinza = document.getElementById("escala_cinza").value;
+
+    document.getElementById("imagem").style.filter= "grayscale("+cinza+"%)";
+ 
 });
 
+$(document).on("change","#opacidade",function(){
 
-$(document).on("click","#codigo",function()
-{
-  cordova.plugins.barcodeScanner.scan(
-      function (result) {
-         if(result.text == 280720550) {
-           $(location).attr("href","Ex1.html");
-         }
-      },
-      function (error) {
-          alert("Scanning failed: " + error);
-      },
-      {
-          preferFrontCamera : false, // iOS and Android
-          showFlipCameraButton : true, // iOS and Android
-          showTorchButton : true, // iOS and Android
-          torchOn: false, // Android, launch with the torch switched on (if available)
-          saveHistory: true, // Android, save scan history (default false)
-          prompt : "Place a barcode inside the scan area", // Android
-          resultDisplayDuration: 500, // Android, display scanned text for X ms. 0 suppresses it entirely, default 1500
-          formats : "QR_CODE,PDF_417,CODE_39", // default: all but PDF_417 and RSS_EXPANDED
-          orientation : "portrait", // Android only (portrait|landscape), default unset so it rotates with the device
-          disableAnimations : true, // iOS
-          disableSuccessBeep: false // iOS and Android
-      }
-  )
+  var opacidade = document.getElementById("opacidade").value;
 
+    document.getElementById("imagem").style.filter= "opacity("+opacidade+"%)";
+ 
+});
+
+$(document).on("change","#saturacao",function(){
+
+  var saturacao = document.getElementById("saturacao").value;
+
+   document.getElementById("imagem").style.filter= "saturate("+saturacao+"%)";
+ 
+});
+
+$(document).on("change","#blur",function(){
+
+  var blur = document.getElementById("blur").value;
+
+    document.getElementById("imagem").style.filter= "blur("+blur+"px)";
+ 
 });
